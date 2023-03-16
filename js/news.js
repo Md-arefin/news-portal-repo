a37f3bd402d8ed3f42418727b319214f51378e83
Md-arefin <ansariarefin617@gmail.com>
2023-03-17 01:51:50 +0600

    first commit

--- js/news.js
+++ js/news.js
@@ -1,15 +1,29 @@
-const fetchCategories = () =>{
+// tailwind 17/03/23
+// async wait
+
+const fetchCategories = () => {
     fetch("https://openapi.programming-hero.com/api/news/categories")
-    .then(res => res.json())
-    .then(data => showCategories(data.data))
+        .then(res => res.json())
+        .then(data => showCategories(data.data))
 }
 
-const showCategories = data =>{
-// capture categories container to append all the categories links
-const categoriesContainer = document.getElementById('categories-container');
-data.news_category.forEach(singleNews =>{
-    categoriesContainer.innerHTML +=`
-    <a class="nav-link active" aria-current="page" href="#">${singleNews.category_name}</a>
+//  show categories data
+
+const showCategories = data => {
+    // capture categories container to append all the categories links
+    const categoriesContainer = document.getElementById('categories-container');
+    data.news_category.forEach(singleNews => {
+        categoriesContainer.innerHTML += `
+    <a class="nav-link active" aria-current="page" href="#" onclick="fetchCategoryNews('${singleNews.category_id}')">${singleNews.category_name}</a>
     `;
-})
+    })
+}
+
+// fetch all newses available in a category
+
+const fetchCategoryNews =category_id =>{
+    const url =(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data.data))
 }
